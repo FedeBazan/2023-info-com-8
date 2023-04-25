73084bf8338296a305db9efceb3a74ff18bdf4af
FedeBazan <juliopablofedericobazan@gmail.com>
2023-04-25 12:07:33 -0300

    Fede Bazan

--- Semana2/EjerciciosPropuestos/ejercicio10.py
+++ Semana2/EjerciciosPropuestos/ejercicio10.py
@@ -1,3 +1,12 @@
 '''10-Escribir un programa que pida al usuario una letra y muestre por pantalla si es
 una vocal o una consonante.
 '''
+letra=input('Escriba una letra: ')
+
+if (ord(letra)>=65 and ord(letra)<=90) or (ord(letra)>=97 and ord(letra)<=122):
+    if letra.lower()=='a' or letra.lower()=='e' or letra.lower()=='i' or letra.lower()=='o' or letra.lower()=='u':
+        print(f'{letra} es una VOCAL')
+    else:
+        print(f'{letra} es una CONSONANTE')
+else:
+    print('NO es una letra')
--- Semana2/EjerciciosPropuestos/ejercicio11.py
+++ Semana2/EjerciciosPropuestos/ejercicio11.py
@@ -1,2 +1,8 @@
 '''11-Escribir un programa que pida al usuario dos números y muestre por pantalla
 la suma de ellos solo si ambos son pares.'''
+
+num1=int(input('Escribe un numero: '))
+num2=int(input('Escribe un numero: '))
+
+if num1%2==0 and num2%2==0:
+    print(f'la suma entre los numeros es {num1+num2}')
