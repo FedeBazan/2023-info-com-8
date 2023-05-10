6cae0d9eec2aeb576f0252a4c01f4d731ccba01b
FedeBazan <juliopablofedericobazan@gmail.com>
2023-05-10 10:25:12 -0300

    Fede Bazan

--- Semana4/EjerciciosPropuestos/ejercicio10.py
+++ Semana4/EjerciciosPropuestos/ejercicio10.py
@@ -3,3 +3,9 @@
 parámetro y devuelva el factorial de ese número. El factorial de un número
 entero positivo n se define como el producto de todos los números enteros
 positivos desde 1 hasta n.'''
+
+def calcular_factorial(numero):
+    continue
+
+numero=int(input('Coloque un numero para obtener el factorial del mismo: '))
+calcular_factorial(numero)
