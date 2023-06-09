7173e5b1e3df26afa6a8e1abd440e4e786d31f0e
FedeBazan <juliopablofedericobazan@gmail.com>
2023-06-09 14:14:46 -0300

    fede baz

--- Semana8/EjerciciosPropuestos/Ejercicio4.py
+++ Semana8/EjerciciosPropuestos/Ejercicio4.py
@@ -8,3 +8,97 @@ momento definiendo de tipo “private”. Crear una clase prueba_tiempo que
 prueba una hora concreta y la modifique a su gusto mostrándola por pantalla.
 
 '''
+import datetime
+import re
+
+'''
+class Tiempo:
+    def __init__(self, hora, minuto=0, segundo=0):
+        self.__hora = hora
+        self.__minuto = minuto
+        self.__segundo = segundo
+
+    def modificar_hora(self, hora, minuto=0, segundo=0):
+        self.__hora = hora
+        self.__minuto = minuto
+        self.__segundo = segundo
+
+    def obtener_hora(self):
+        return self.__hora
+
+    def obtener_minuto(self):
+        return self.__minuto
+
+    def obtener_segundo(self):
+        return self.__segundo
+'''
+
+class PruebaTiempo:
+    def __init__(self):
+        self.tiempo = datetime.datetime.now()  # Obtener la hora actual
+
+    def modificar_tiempo(self):
+        hora = self.__ingresar_hora()
+        minuto = self.__ingresar_minuto()
+        segundo = self.__ingresar_segundo()
+        self.tiempo = self.tiempo.replace(hour=hora, minute=minuto, second=segundo)  # Modificar la hora
+
+    def __ingresar_hora(self):
+        hora = int(input("Ingrese la hora: "))
+        while not (0 <= hora <= 23):
+            print("El valor de la hora debe estar entre 0 y 23.")
+            hora = int(input("Ingrese la hora nuevamente: "))
+        return hora
+
+    def __ingresar_minuto(self):
+        minuto = int(input("Ingrese el minuto: "))
+        while not (0 <= minuto <= 59):
+            print("El valor del minuto debe estar entre 0 y 59.")
+            minuto = int(input("Ingrese el minuto nuevamente: "))
+        return minuto
+
+    def __ingresar_segundo(self):
+        segundo = int(input("Ingrese el segundo: "))
+        while not (0 <= segundo <= 59):
+            print("El valor del segundo debe estar entre 0 y 59.")
+            segundo = int(input("Ingrese el segundo nuevamente: "))
+        return segundo
+
+    def mostrar_tiempo(self):
+        print(f'Hora: {self.tiempo.hour}:{self.tiempo.minute}:{self.tiempo.second}')
+
+
+prueba = PruebaTiempo()
+prueba.mostrar_tiempo()  # Mostrar la hora inicial
+prueba.modificar_tiempo()  # Modificar la hora
+prueba.mostrar_tiempo()  # Mostrar la hora modificada
+}
+
+_date ='201909010900'
+regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9][0-5][0-9][0-5][0-9]")
+_date = re.findall(regex, _date)
+print(_date)
+match = re.match(regex, _date[0])
+if (match):
+     print(_date)
+else:
+    regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9]")    
+    _date = re.findall(regex, _date)
+    match= re.match(regex,_date[0])
+if (match):
+    print(_date)
+else: 
+    regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9][0-5][0-9]")
+    _date = re.findall(regex, _date)
+    match= re.match(regex,_date[0])
+    
+if (match):
+    print(_date)        
+else: 
+    regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9][0-5][0-9][0-5][0-9]")
+    _date = re.findall(regex, _date)
+    match= re.match(regex,_date[0])
+if (match):
+    print(_date)
+else: 
+    print("Invalid date, the format has to be YYYYMMDDHHMMSS or YYYYMMDD or YYYYMMDDHHMM or YYYYMMDDHH")
