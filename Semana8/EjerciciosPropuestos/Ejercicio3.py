f7a8fbdef744031e4933b4539f01d56bbbc7890f
FedeBazan <juliopablofedericobazan@gmail.com>
2023-06-16 00:22:56 -0300

    Fede Bazan
    ejercicio a revision

--- Semana8/EjerciciosPropuestos/Ejercicio3.py
+++ Semana8/EjerciciosPropuestos/Ejercicio3.py
@@ -1,7 +1,7 @@
 '''
 Ejercicio 3: Triángulo
 Desarrollar un programa que cargue los datos de un triángulo.
-Implementar una clase con los métodos para inicializar los atributos, imprimir el
+Implementar   una clase con los métodos para inicializar los atributos, imprimir el
 valor del lado con un tamaño mayor y el tipo de triángulo que es (equilátero,
 isósceles o escaleno).
 
--- Semana8/EjerciciosPropuestos/Ejercicio4.py
+++ Semana8/EjerciciosPropuestos/Ejercicio4.py
@@ -74,33 +74,3 @@ prueba = PruebaTiempo()
 prueba.mostrar_tiempo()  # Mostrar la hora inicial
 prueba.modificar_tiempo()  # Modificar la hora
 prueba.mostrar_tiempo()  # Mostrar la hora modificada
-}
-
-_date ='201909010900'
-regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9][0-5][0-9][0-5][0-9]")
-_date = re.findall(regex, _date)
-print(_date)
-match = re.match(regex, _date[0])
-if (match):
-     print(_date)
-else:
-    regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9]")    
-    _date = re.findall(regex, _date)
-    match= re.match(regex,_date[0]) 
-if (match):
-    print(_date)
-else: 
-    regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9][0-5][0-9]")
-    _date = re.findall(regex, _date)
-    match= re.match(regex,_date[0])
-    
-if (match):
-    print(_date)        
-else: 
-    regex = re.compile("[1-2][0,9][0-9]{2}[0,1][0-9][0,1,2,3][0-9][0,1,2][0-9][0-5][0-9][0-5][0-9]")
-    _date = re.findall(regex, _date)
-    match= re.match(regex,_date[0])
-if (match):
-    print(_date)
-else: 
-    print("Invalid date, the format has to be YYYYMMDDHHMMSS or YYYYMMDD or YYYYMMDDHHMM or YYYYMMDDHH")
