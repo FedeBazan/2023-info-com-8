ac26e94445022354c76f65da269d08f9e503ea32
FedeBazan <juliopablofedericobazan@gmail.com>
2023-06-29 07:25:01 -0300

    fede baz
    pull on up

--- Django/Django-Project/SAP/webapp/views.py
+++ Django/Django-Project/SAP/webapp/views.py
@@ -3,16 +3,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def bienvenido(request):
-    return HttpResponse('Hola mundo desde Django')
+    return HttpResponse('''<h1>Hola mundo desde Django</h1>
+    <a link"">''')
 
 def despedida(request):
-    return HttpResponse('Despedida desde Django')
+    return HttpResponse('<h1>Despedida desde Django</h1>')
 
 def contacto(request):
     return HttpResponse('''<h1>CONTACTO</h1>
-    <br>Name: Julio Pablo Federico
-    <br>Surname:Bazan
-    <br>Cell:1233123
-    <br>contact: cell phone aosojkdaklsjdnalsnjd
-    <br>y otras cosas... 
+    <ul>
+        <li>Name: Julio Pablo Federico</li>
+        <li>Surname:Bazan</li>
+        <li>Cell:1233123</li>
+        <li>contact: cell phone aosojkdaklsjdnalsnjd</li>
+        <li>y otras cosas...</li>
+    </ul>
+    
     ''')
